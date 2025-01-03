e8a7a0a121d4afc08c0c29cf6e19d89af5aefc54
Thomas Langé <thomas.lange.oss@gmail.com>
2025-01-03 22:08:01 +0000

    Add basic user & fstab info in wsl profile

--- wsl.nix
+++ wsl.nix
@@ -7,4 +7,18 @@
   system.stateVersion = "24.05";
   wsl.enable = true;
   wsl.defaultUser = "thomas";
+  users.users.thomas = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+    packages = with pkgs; [
+      unzip
+    ];
+    shell = pkgs.zsh;
+  };
+  fileSystems."/mnt/z" = {
+    device = "Z:";
+    fsType = "drvfs";
+  };
 }
