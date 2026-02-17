0da3d87f078de7dc1cadf4ff6b11a13718942235
Thomas Langé <thomas.lange.oss@gmail.com>
2026-02-17 09:58:39 +0100

    Remove temporaty hacks for rofimoji as my patch was merged upstream

--- workstation.nix
+++ workstation.nix
@@ -1,20 +1,9 @@
 { inputs, pkgs, pkgsUnstable, ... }:
-let
-  # workaround waiting for https://github.com/NixOS/nixpkgs/pull/480317
-  my-rofimoji = (pkgsUnstable.rofimoji.overrideAttrs (old: {
-    # Force support of darwin and linux platforms now
-    meta = old.meta // { platforms = pkgs.lib.platforms.linux ++ pkgs.lib.platforms.darwin; };
-  }
-  )).override {
-    waylandSupport = false;
-    x11Support = false;
-  };
-in
 {
   environment.systemPackages = with pkgs; [
     keepassxc
     kitty
-    my-rofimoji
+    pkgsUnstable.rofimoji
 
     # For Neovim/Lazyvim
     neovim # Defined already in ./linux-workstation.nix but needed for darwin too
