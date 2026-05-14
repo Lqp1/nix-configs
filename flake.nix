d137a13b80eff3360f034f19ee64b196d6c2bd68
Thomas Langé <thomas.lange.oss@gmail.com>
2026-05-14 22:30:21 +0200

    Add devshell

--- flake.nix
+++ flake.nix
@@ -4,32 +4,76 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+
     smount.url = "github:lqp1/smount/main";
     smount.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-needsreboot.url = "https://codeberg.org/Mynacol/nixos-needsreboot/archive/main.tar.gz";
     nixos-needsreboot.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, nixpkgs-unstable, nixos-hardware, nixos-wsl, smount, nixos-needsreboot }:
+  outputs =
+    inputs@{ self
+    , nixpkgs
+    , nixpkgs-unstable
+    , nix-darwin
+    , nixos-hardware
+    , flake-utils
+    , smount
+    , nixos-needsreboot
+    , ...
+    }:
     let
       unstableOverlay = final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
+        unstable = import nixpkgs-unstable {
           inherit (prev.stdenv.hostPlatform) system;
           config.allowUnfree = true;
         };
       };
-      overlayModule = { nixpkgs.overlays = [ unstableOverlay ]; };
-    in
-    {
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
 
+      overlayModule = {
+        nixpkgs.overlays = [ unstableOverlay ];
+      };
+    in
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ unstableOverlay ];
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deadnix
+            omnix
+          ];
+
+          shellHook = ''
+            echo "Nix config dev shell (${system})"
+            echo "Useful commands:"
+            echo "  nix fmt"
+            echo "  deadnix ."
+            echo "  statix check ."
+            echo "  nix flake check"
+            echo "  om ci"
+          '';
+        };
+      })
+    // {
       darwinConfigurations."FV3Y4FYJ31" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
         modules = [
           overlayModule
           ./os/darwin-base.nix
@@ -39,7 +83,9 @@
         ];
         specialArgs = { inherit inputs; };
       };
+
       nixosConfigurations."thomas-x201" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
           overlayModule
           ./os/linux-base.nix
@@ -51,7 +97,9 @@
         ];
         specialArgs = { inherit inputs; };
       };
+
       nixosConfigurations."thomas-t460" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
           overlayModule
           ./os/linux-base.nix
@@ -63,7 +111,9 @@
         ];
         specialArgs = { inherit inputs; };
       };
+
       nixosConfigurations."thomas-desktop" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
           overlayModule
           ./os/linux-base.nix
