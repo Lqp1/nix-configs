f3654ab66851019304eff67828c40625383b3fa8
Thomas Langé <thomas.lange.oss@gmail.com>
2026-08-09 21:22:58 +0200

    Fix CI

--- hacks.nix
+++ hacks.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, options, ... }:
 
 {
   options = {
@@ -11,37 +11,44 @@
     my.sudoWrapper = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description = "Non-SUID sudo wrapper pointing to run0 for tools with hardcoded sudo calls (e.g. VeraCrypt)";
+      description = "Non-SUID sudo wrapper pointing to pkexec for tools with hardcoded sudo calls (e.g. VeraCrypt)";
     };
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (config.my.fprintMitigation && pkgs.stdenv.isLinux) {
-      # Prevent USB autosuspend and disconnects on the Synaptics Prometheus sensor
-      services.udev.extraRules = ''
+    (lib.optionalAttrs (options ? services && options.services ? udev) {
+      services.udev.extraRules = lib.mkIf config.my.fprintMitigation ''
         ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="06cb", ATTR{idProduct}=="00fc", ATTR{power/control}="on"
       '';
+    })
 
-      services.tlp.settings = {
+    (lib.optionalAttrs (options ? services && options.services ? tlp) {
+      services.tlp.settings = lib.mkIf config.my.fprintMitigation {
         USB_DENYLIST = "06cb:00fc";
       };
+    })
 
-      systemd.services.fprintd.serviceConfig = {
+    (lib.optionalAttrs (options ? systemd && options.systemd ? services) {
+      systemd.services.fprintd.serviceConfig = lib.mkIf config.my.fprintMitigation {
         TimeoutStopSec = "2s";
         Restart = "on-failure";
       };
+    })
 
-      powerManagement.resumeCommands = ''
+    (lib.optionalAttrs (options ? powerManagement) {
+      powerManagement.resumeCommands = lib.mkIf config.my.fprintMitigation ''
         ${pkgs.systemd}/bin/systemctl try-restart fprintd.service || true
       '';
     })
 
-    (lib.mkIf (config.my.sudoWrapper && pkgs.stdenv.isLinux) {
-      security.wrappers.sudo = {
-        owner = "root";
-        group = "root";
-        setuid = false;
-        source = "${pkgs.writeScript "sudo" (builtins.readFile ./files/sudo)}";
+    (lib.optionalAttrs (options ? security && options.security ? wrappers) {
+      security.wrappers = lib.mkIf config.my.sudoWrapper {
+        sudo = {
+          owner = "root";
+          group = "root";
+          setuid = false;
+          source = "${pkgs.writeScript "sudo" (builtins.readFile ./files/sudo)}";
+        };
       };
     })
   ];
